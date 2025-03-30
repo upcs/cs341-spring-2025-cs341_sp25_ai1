710e1339458f257c1535a36632ea80f35b7e4fd9
Danie1Le <danielle8262005@gmail.com>
2025-03-30 14:43:10 -0700

    Delete hide notes button

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -94,9 +94,9 @@ const generateResponse = (incomingChatLI) => {
         notesPanel.classList.toggle('hidden');
     }
 
-    // event listeners for notes
-    notesBtn.addEventListener('click', toggleNotes);
-    closeNotesBtn.addEventListener('click', toggleNotes);
+    // // event listeners for notes
+    // notesBtn.addEventListener('click', toggleNotes);
+    // closeNotesBtn.addEventListener('click', toggleNotes);
 
     // make new log
     const newChatBtn = document.querySelector('#new-chat-btn');
