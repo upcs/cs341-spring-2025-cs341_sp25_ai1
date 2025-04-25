9983ebe525e0c97df3f9f09441fb97d76cb0b9e0
ChrisVo128 <ctinvo101@gmail.com>
2025-04-24 22:45:39 -0700

    test files for js files

--- routes/caseChooser.js
+++ routes/caseChooser.js
@@ -8,9 +8,14 @@ function selectCollectionName() {
 
 router.post('/', function(req, res) {
     collectionName = selectCollectionName();
-    
+
     //hardcoded-for case #1 at the moment
     res.json({"case": collectionName});
 });
+
+// Export both the router and the selectCollectionName function
+router.routes = {
+    selectCollectionName: selectCollectionName
+};
 
 module.exports = router;
